b9633800b660ed462bc07c65de2acbdc08ee27e2
Kayla <6lackmacaroon@gmail.com>
2019-06-25 09:03:05 -0600

    update readme - begin ID routes

--- api/inmate-router.js
+++ api/inmate-router.js
@@ -1,34 +1,44 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Inmates = require('./inmate-model.js');
-const { restricted } = require('../auth/auth-middleware.js');
+const Inmates = require("./inmate-model.js");
+const { restricted } = require("../auth/auth-middleware.js");
 
 // /api/inmates
 
-router.get('/', async (req, res) => {
-            try {
-                  const inmates = await Inmates.find('inmates')
-                  res.status(200).json(inmates)
-            } catch (error) {
-                  console.log(error);
-                  res.status(500).json({ error: 'failed to retrieve inmate profiles from the database'})
-            }
-      })
-      .post('/', async (req, res) => {
-            const inmate = req.body
-            if(!inmate.name || !inmate.skills){
-                  return res.status(400).json({message: 'all fields are required'});  
-            } 
-            try {
-                  const newInmate = await db.add(inmate)
-                  res.status(201).json({ message: 'inmate profile created'})
-            } catch (error) {
-                  console.log(error);
-                  res.status(500).json({ error: 'failed adding inmate profile to the database'});
-            }
-            
-      })
+router
+  .get("/", async (req, res) => {
+    try {
+      const inmates = await Inmates.find("inmates");
+      res.status(200).json(inmates);
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({
+          error: "failed to retrieve inmate profiles from the database"
+        });
+    }
+  })
+  .post("/", async (req, res) => {
+    const inmate = req.body;
+    if (!inmate.name || !inmate.skills) {
+      return res.status(400).json({ message: "all fields are required" });
+    }
+    try {
+      const newInmate = await db.add(inmate);
+      res.status(201).json({ message: "inmate profile created" });
+    } catch (error) {
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "failed adding inmate profile to the database" });
+    }
+  });
 
+// /api/inmates/:id
 
+router.get('/:id', async (req, res) => {
+      
+})
 
 module.exports = router;
