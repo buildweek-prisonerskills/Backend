6207fddc240e62a07f5b7dee55494d6274b5fb3c
Kayla <6lackmacaroon@gmail.com>
2019-06-25 11:49:59 -0600

    created getById routes for facilities

--- api/inmate-router.js
+++ api/inmate-router.js
@@ -10,13 +10,11 @@ router
     try {
       const inmates = await Inmates.find("inmates");
       res.status(200).json(inmates);
-    } catch (error) {
-      console.log(error);
-      res
-        .status(500)
-        .json({
-          error: "failed to retrieve inmate profiles from the database"
-        });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        error: "failed to retrieve inmate profiles from the database"
+      });
     }
   })
   .post("/", async (req, res) => {
@@ -27,8 +25,8 @@ router
     try {
       const newInmate = await Inmates.add(inmate);
       res.status(201).json({ message: "inmate profile created" });
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log(err);
       res
         .status(500)
         .json({ error: "failed adding inmate profile to the database" });
@@ -37,45 +35,62 @@ router
 
 // /api/inmates/:id
 
-router.get('/:id', (req, res) => {
-            Inmates.findById(req.params.id)
-            .then(inmate => {
-                  if(req.params.id) {
-                        res.status(200).json(inmate);
-                  } else {
-                        res.status(404).json({ message: "no inmate with that ID currently exists in our database"})
-                  }
-            })
-            .catch(err => {
-                  console.log(err);
-                  res.status(500).json({ message: "could not retrieve inmates from our database"})
-            })
+router
+  .get("/:id", (req, res) => {
+    Inmates.findById(req.params.id)
+      .then(inmate => {
+        if (req.params.id) {
+          res.status(200).json(inmate);
+        } else {
+          res
+            .status(404)
+            .json({
+              message: "no inmate with that ID currently exists in our database"
+            });
+        }
       })
-      .put('/:id', async (req, res) => {
-            try {
-                  const change = await Inmates.update(req.params.id, req.body);
-                  if(change) {
-                        res.status(200).json({ message: 'update successful' });
-                  } else {
-                        res.status(404).json({ message: 'that inmate could not be found in our database' })
-                  }
-            } catch(err) {
-                  console.log(err)
-                  res.status(500).json({ message: 'could not update the inmates database'})
-            }
-      })
-      .delete('/:id', async (req, res) => {
-            try {
-                  const count = await Inmates.remove(req.params.id);
-                  if(count > 0) {
-                        res.status(200).json({message: 'this inmate has been removed from the database'})
-                  } else {
-                        res.status(404).json({ message: 'that inmate could not be found in our database'})
-                  }
-            } catch(err) {
-                  console.log(err)
-                  res.status(500).json({ message: 'could not remove inmate from database'})
-            }
-      })
+      .catch(err => {
+        console.log(err);
+        res
+          .status(500)
+          .json({ message: "could not retrieve inmates from our database" });
+      });
+  })
+  .put("/:id", async (req, res) => {
+    try {
+      const change = await Inmates.update(req.params.id, req.body);
+      if (change) {
+        res.status(200).json({ message: "update successful" });
+      } else {
+        res
+          .status(404)
+          .json({ message: "that inmate could not be found in our database" });
+      }
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "could not update the inmates database" });
+    }
+  })
+  .delete("/:id", async (req, res) => {
+    try {
+      const count = await Inmates.remove(req.params.id);
+      if (count > 0) {
+        res
+          .status(200)
+          .json({ message: "this inmate has been removed from the database" });
+      } else {
+        res
+          .status(404)
+          .json({ message: "that inmate could not be found in our database" });
+      }
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "could not remove inmate from database" });
+    }
+  });
 
 module.exports = router;
