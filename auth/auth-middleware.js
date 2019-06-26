315fa68ea51631b60d3b4261389fdd2b5730082b
Kayla <6lackmacaroon@gmail.com>
2019-06-26 11:46:11 -0600

    created validate login and validate register middleware

--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -4,10 +4,12 @@ const jwtKey = process.env.JWT_SECRET || 'add a .env file to root of project wit
 
 module.exports = {
   authenticate,
-  generateToken
+  generateToken,
+  validateLogin,
+  validateRegister
 };
 
-// implementation details
+// authenticate a user is logged in by decoding their web token
 function authenticate(req, res, next) {
   const token = req.get('Authorization');
 
@@ -26,6 +28,7 @@ function authenticate(req, res, next) {
   }
 }
 
+//create web token on login
 function generateToken(user) {
   const payload = {
     subject: user.id,
@@ -36,4 +39,20 @@ function generateToken(user) {
   };
   const secret = jwtKey;
   return jwt.sign(payload, secret, options)
+}
+
+function validateLogin(req, res, next){
+  let {username, password} = req.body;
+  if(!username || !password){
+    return res.status(412).json({ message: 'both username and password are required' })
+  }
+  next()
+}
+
+function validateRegister(req, res, next) {
+  let {username, password, facility_id} = req.body;
+  if(!username || !password || !facility_id){
+    return res.status(412).json({ message: 'all fields are required... username, password, facility id' })
+  }
+  next()
 }
